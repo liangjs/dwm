20dc4d2d0229b09b2b30539b672344c50a5fec7b
liangjs <761232680@qq.com>
2020-10-05 17:33:16 +0800

    layout menu

--- dwm.c
+++ dwm.c
@@ -190,6 +190,7 @@ static void grabkeys(void);
 static void incnmaster(const Arg *arg);
 static void keypress(XEvent *e);
 static void killclient(const Arg *arg);
+static void layoutmenu(const Arg *arg);
 static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
@@ -1731,6 +1732,24 @@ setup(void)
 	focus(NULL);
 }
 
+void
+layoutmenu(const Arg *arg)
+{
+	FILE *p;
+	char c[3], *s;
+	int i;
+
+	if (!(p = popen(layoutmenu_cmd, "r")))
+		 return;
+	s = fgets(c, sizeof(c), p);
+	pclose(p);
+
+	if (!s || *s == '\0' || c == '\0')
+		 return;
+
+	i = atoi(c);
+	setlayout(&((Arg) { .v = &layouts[i] }));
+}
 
 void
 seturgent(Client *c, int urg)
